dba5afa8c8a0b6c0b75d889dbe94cce438e2772f
thienlai <giathien752005@gmail.com>
2026-06-21 15:29:47 +0700

    fix: modify app config

--- backend/src/main/java/com/ticketapp/config/ApplicationConfig.java
+++ backend/src/main/java/com/ticketapp/config/ApplicationConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.client.RestClient;
 
 @Configuration
 public class ApplicationConfig {
@@ -11,5 +12,10 @@ public class ApplicationConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public RestClient.Builder restClientBuilder() {
+        return RestClient.builder();
     }
 }
